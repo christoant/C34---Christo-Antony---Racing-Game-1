c44f0cf2d1da198f59453d928e8f99dd2a9eccf2
christoant <67069320+christoant@users.noreply.github.com>
2020-07-28 12:29:02 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C34---Christo-Antony---Racing-Game-1
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,44 @@
+var ball;
+var database, position, balllocation;
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    balllocation = database.ref("ball/positions");
+    balllocation.on("value",readop,showerror);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("ball/positions").set({
+        x:ball.x + x,
+        y:ball.y + y
+    });   
+}
+
+function readop(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showerror(){
+    console.log("error");
+}
